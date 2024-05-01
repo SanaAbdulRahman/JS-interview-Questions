57560155a55260f83d9542ed663d5399ff6fa3f4
sanath <sanaabdulmanaf88@gmail.com>
2024-05-01 09:57:52 +0400

    different methods for timer using 'var' and 'let'

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+//by using 'var'
+// function timer() { }
+// for (var i = 1; i <= 5; i++) {
+//     setTimeout(function () {
+//         console.log(i);
+//     }, i * 1000);
+// }
+// timer();
+//output will be : prints 6 in 5 times in each interval. Follow below code to get the expected output. it done by the use of closure
+
+
+//the use of closure will help us to get the expected output even by using 'var'keyword.
 function timer() {
-    for (let i = 1; i <= 10; i++) {
-        setTimeout(function () {
-
-            console.log(i);
-
-        }, i * 1000);
+    for (var i = 1; i <= 10; i++) {
+        function close(i) {
+            setTimeout(function () {
+                console.log(i);
+            }, i * 1000);
+        }
+        close(i);
     }
 }
 timer();
+
+//by using'let'
+// function timer() {
+//     for (let i = 1; i <= 10; i++) {
+//         setTimeout(function () {
+
+//             console.log(i);
+
+//         }, i * 1000);
+//     }
+// }
+// timer();
+
+
